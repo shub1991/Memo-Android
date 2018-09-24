f56b25806392557a42b3bdafa4243cf4e14525d7
Shubham Basu <shubham.basu991@gmail.com>
2018-09-23 22:47:30 -0400

    deleting element

--- app/src/main/java/com/pervasive/memo/memo/MemoList.java
+++ app/src/main/java/com/pervasive/memo/memo/MemoList.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +28,7 @@ public class MemoList extends AppCompatActivity {
     TextView name, email, phNo;
     Button add;
     FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(this);
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +37,59 @@ public class MemoList extends AppCompatActivity {
         phNo = (TextView) findViewById(R.id.memo_phNo);
         email = (TextView) findViewById(R.id.memo_email);
         add = (Button)findViewById(R.id.add);
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from Memos",null);
+        ArrayList<String> maintitle = new ArrayList<>();
+        ArrayList<String> subtitle = new ArrayList<>();
+        ArrayList<Integer> ids = new ArrayList<>();
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                String name = cursor.getString(cursor.getColumnIndex("title"));
+                String name2 = cursor.getString(cursor.getColumnIndex("desc"));
+                Integer name3 = cursor.getInt(cursor.getColumnIndex("_id"));
+
+                maintitle.add(name);
+                subtitle.add(name2);
+                ids.add(name3);
+                cursor.moveToNext();
+            }
+        }
+
+        MyListAdapter adapter=new MyListAdapter(this, maintitle, subtitle, ids);
+        list=(ListView)findViewById(R.id.list_view);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+
+                Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                if(position == 0) {
+                    //code specific to first list item
+                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
     @Override
@@ -56,18 +111,7 @@ public class MemoList extends AppCompatActivity {
                 cursor.moveToNext();
             }
         }
-        final ArrayAdapter adapter = new ArrayAdapter<String>(MemoList.this,R.layout.list_item,str);
-        ListView listView = (ListView) findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-        Toast.makeText(MemoList.this,getIntent().getStringExtra("Name") , Toast.LENGTH_SHORT).show();
-        add.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MemoList.this, MemoEdit.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
---
+++ app/src/main/java/com/pervasive/memo/memo/MyListAdapter.java
@@ -0,0 +1,62 @@
+package com.pervasive.memo.memo;
+
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by shubh on 9/23/2018.
+ */
+
+public class MyListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final ArrayList<String> maintitle;
+    private final ArrayList<String> subtitle;
+    private final ArrayList<Integer> ids;
+    FeedReaderDbHelper mDbHelper;
+    SQLiteDatabase db;
+
+    public MyListAdapter(Activity context, ArrayList<String> maintitle, ArrayList<String> subtitle, ArrayList<Integer> ids) {
+        super(context, R.layout.list_item, maintitle);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.maintitle=maintitle;
+        this.subtitle=subtitle;
+        this.ids=ids;
+        mDbHelper =  new FeedReaderDbHelper(context);
+
+        db = mDbHelper.getWritableDatabase();
+    }
+
+    public View getView(final int position, View view, ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_item, null,true);
+
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        TextView subtitleText = (TextView) rowView.findViewById(R.id.subtitle);
+        Button deleteBtn = (Button) rowView.findViewById(R.id.deleteBtn);
+        titleText.setText(maintitle.get(position));
+        subtitleText.setText(subtitle.get(position));
+
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, position+"-->"+ids.get(position),Toast.LENGTH_LONG).show();
+                db.delete("Memos","_id" + "=" + ids.get(position), null);
+            }
+        });
+        return rowView;
+
+    };
+}
